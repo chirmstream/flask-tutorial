6cc0dc0bf540cc3ad3c3bb744d83f43c7a426fe8
chirmstream <33538092+chirmstream@users.noreply.github.com>
2023-03-18 23:33:44 -0700

    Created virtual enviroment.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -7,7 +7,9 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
+        # a default secret that should be overridden by instance config
         SECRET_KEY='dev',
+        # store the database in the instance folder
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
@@ -29,7 +31,7 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
-    from . import db
+    from . flaskr import db
     db.init_app(app)
     
     return app
